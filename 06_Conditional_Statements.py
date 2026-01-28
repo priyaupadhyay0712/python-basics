5fdaba3da645c936d6fb4f8fbff8580c37b8cdcc
priyaupadhyay0712 <priya.upadhyay0712@gmail.com>
2026-01-28 21:13:22 +0530

    Correct indentation in pass/fail logic
    Fix indentation error in conditional statements example.

---
+++ 06_Conditional_Statements.py
@@ -0,0 +1,10 @@
+#Conditional statements(if,else)
+#Comman error here is ** IndentationError **
+#Pass OR Fail
+
+avg = float(input("Enter average marks: ")
+
+            if avg >= 40:            #In Python, indentation is mandatory and used to define code blocks.Here (:) is used to define the function and is signal the start of an indented bloack on the next line.
+               print("PASSED")
+            else:
+               print("FAILED")
