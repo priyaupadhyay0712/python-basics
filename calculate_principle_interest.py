b7e27396169deebdb82ec9f461b148c053cec9b4
priyaupadhyay0712 <priya.upadhyay0712@gmail.com>
2026-01-28 22:18:10 +0530

    Delete calculate_principle_interest.py

--- calculate_principle_interest.py
+++
@@ -1,9 +0,0 @@
-p = float(input("Enter the principal:"))
-r = float(input("Enter rate of interest:"))
-t = float(input("Enter Time period(in years):"))
-
-si = (p*r*t)/100
-amount = p*(1+r/100) ** t
-ci = amount - p
-print("Simple Interest:",si)
-print("Compound Interest:",ci)
