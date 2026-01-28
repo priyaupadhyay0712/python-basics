eebdc6d38ba1b8182a0bd000b0779d0766380d57
priyaupadhyay0712 <priya.upadhyay0712@gmail.com>
2026-01-28 20:49:14 +0530

    03_hello_username.py for user greeting
    Add a script to greet the user by name.

---
+++ 03_hello_username.py
@@ -0,0 +1,7 @@
+#Declaring Variable.
+#Variable is a Container that stores data of any datatypes.
+#variable is a case sensitive,it can ne declared using alphanumeric,and special symbol used here is underscore(_).
+#Datatype is defined as the type of variable store i.e it can be string,int,boolean,float.
+#Here name is a variable,name is string datatypes.
+name = input("Enter your name: ") 
+print("Hello " + name)
