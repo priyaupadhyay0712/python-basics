ad75a5834eed13a380a74da73fa85f558ce4f013
priyaupadhyay0712 <priya.upadhyay0712@gmail.com>
2026-01-28 21:05:15 +0530

    to calculate average of three numbers

---
+++ 05_avg_3_num.py
@@ -0,0 +1,7 @@
+#Calculating Average of 3 Numbers wrtten using Grades.
+m1 = float(input("Enter marks 1: ")
+m2 = float(input("Enter marks 2: ")
+m3 = float(input("Enter marks 3: ")
+
+avg = (m1 + m2 + m3) / 3
+print("Average =", avg)
