104beafc5fae6b869406361dc33d559a65d423e8
priyaupadhyay0712 <priya.upadhyay0712@gmail.com>
2026-01-31 00:37:51 +0530

    Break and Continue

---
+++ break_continue.py
@@ -0,0 +1,16 @@
+#Break_Continue -
+#Break - It terminates the statements, controls the flow of program.
+#1.BREAK
+#for val in "College":
+   # if val == " ":
+    #   break
+    #print(val)
+    #print("The End")
+
+
+#2.CONTINUE
+#for letter in "Python":
+ # if letter == "h":
+  #    continue
+   # print("Current Letter:",letter)
+    
